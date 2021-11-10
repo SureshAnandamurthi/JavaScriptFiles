c4e3404e6bb4672119640718052dc28f61ced808
SureshAnandamurthi <suresh.anandamurthi@gmail.com>
2021-11-10 21:37:43 +0530

    updated more lines

--- JSBasics.js
+++ JSBasics.js
@@ -16,12 +16,47 @@ let isLoanApproved = true; // Boolean literal
 let firstName; // default value is undefined
 let firstName=undefined; // undefined
 let selectedColor = null;
-
+let num = 2154642316546464546n // bigint
 typeof(age) // Number
 typeof(name) // String
 typeof(isLoanApproved) // Boolean
 typeof(firstName) // undefined is value as well as data type
 typeof(selectedColor) // Object
+
+let num1 = 10;
+let num2 = 2;
+let res = num1 ** num2
+num1 = -num1 // value would be converted into negative value
+
+let boolValue = true;
+boolValue = +boolValue; // displays 1 which represents the True
+
+let numString = "3";
+numString = +numString; //converts the data type from string to number as the value is number.
+Number(numString) // this also converts data type from string to number as the value is number.
+
+let age= 30; 
+age.toString() // to convert the number data type to String datatype
+
+let stringText = "This is not good at all";
+stringText.length-1 // as the index starts from 0
+stringText.indexOf("not");
+stringText.slice(12,16);
+stringText.slice(6,);
+stringText.toUpperCase();
+stringText.toLowerCase();
+
+let cities = ["mumBai", "DELHi", "JaiPuR", "gOA"];
+
+for (let city of cities){
+    let cityLowerCase = city.toLowerCase();
+    let firstLChar = cityLowerCase[0];
+    cityLowerCase = cityLowerCase.replace(firstLChar, firstLChar.toUpperCase());
+    console.log(cityLowerCase);
+}
+
+
+console.log(`${} Good Morning`)
 
 //Reference Types(object, Array, Function
 let person = {} // object leteral
