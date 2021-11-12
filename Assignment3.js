ffa28e94401006d32b08ac512caff835a46c9bca
SureshAnandamurthi <suresh.anandamurthi@gmail.com>
2021-11-12 10:35:44 +0530

    new file

---
+++ Assignment3.js
@@ -0,0 +1,11 @@
+// 12. Write a program to print reverse of digits of numbers
+function reversed(val){
+    let num = String(val)
+    let len = num.length-1
+    let reversed;
+    for (let i = num.length-1; i>=0; i--){
+            console.log(num[i]);        
+    }
+}
+
+reversed(123456);
